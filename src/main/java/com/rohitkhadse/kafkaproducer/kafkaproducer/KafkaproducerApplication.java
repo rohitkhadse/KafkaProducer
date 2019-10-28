ef7ee860353e6f4bef2e7fad82b4c3eb2cb20074
rohitkhadse <rohit.a.khadse@gmail.com>
2019-10-27 22:17:09 -0400

    initial commit

---
+++ src/main/java/com/rohitkhadse/kafkaproducer/kafkaproducer/KafkaproducerApplication.java
@@ -0,0 +1,13 @@
+package com.rohitkhadse.kafkaproducer.kafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaproducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohitkhadse/kafkaproducer/kafkaproducer/KafkaproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohitkhadse.kafkaproducer.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
